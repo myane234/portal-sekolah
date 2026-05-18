cc213c3b078b747eca8e0d43a076aef61ad7d1f9
faruq <flytothemoonkawaii@gmail.com>
2026-05-18 13:24:06 +0700

    edit

--- src/utils/groqBot.js
+++ src/utils/groqBot.js
@@ -1,7 +1,7 @@
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://api.faaruq.com/api";
 
 export async function askGroq(prompt) {
-  const response = await fetch(`${BACKEND_URL}/chat`, {
+  const response = await fetch(`${BACKEND_URL}/api/chat`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
